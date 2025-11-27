364b0cbdead2d6b3a7c983840a5363a296912908
Kush Rathod <kush.rathod@deptagency.com>
2025-11-27 11:54:45 +0530

    css class add

--- blocks/fund-video/fund-video.js
+++ blocks/fund-video/fund-video.js
@@ -6,16 +6,25 @@ export default function decorate(block) {
   // Embed(block.children[1].querySelector('.button-container'));
   dataMapMoObj.CLASS_PREFIXES = ['fundvedmain', 'fundvedsubmain', 'fundvedinnermain', 'fundsubvedmain'];
   dataMapMoObj.addIndexed(block);
+  try {
+    const imgWrapper = document.createElement('div');
+    imgWrapper.classList.add('fundsubinner');
+    const fundv2 = block.querySelector('.fundvedmain2');
+    const fundvedone = block.querySelector('.fundvedmain2 .fundvedinnermain1');
+    const fundvedtwo = block.querySelector('.fundvedmain2 .fundvedinnermain2');
+    imgWrapper.appendChild(fundvedone);
+    imgWrapper.appendChild(fundvedtwo);
+    fundv2.prepend(imgWrapper);
 
-  const imgWrapper = document.createElement('div');
-  imgWrapper.classList.add('fundsubinner');
-  const fundv2 = block.querySelector('.fundvedmain2');
-  const fundvedone = block.querySelector('.fundvedmain2 .fundvedinnermain1');
-  const fundvedtwo = block.querySelector('.fundvedmain2 .fundvedinnermain2');
-  imgWrapper.appendChild(fundvedone);
-  imgWrapper.appendChild(fundvedtwo);
-  fundv2.prepend(imgWrapper);
+    const imgAltmain = block.closest('main');
+    dataMapMoObj.altFunction(imgAltmain.querySelector('.fund-video-container .fundvedmain2 .fundvedinnermain2 img'), 'video-play-btn');
+  } catch (error) {
+    // console.log(error);
+  }
 
-  const imgAltmain = block.closest('main');
-  dataMapMoObj.altFunction(imgAltmain.querySelector('.fund-video-container .fundvedmain2 .fundvedinnermain2 img'), 'video-play-btn');
+  if (window.location.href.includes('/static-pages/elss-fund')) {
+    const fundTax = block.closest('main').querySelector('.section.fund-tax');
+    dataMapMoObj.CLASS_PREFIXES = ['fund-tax-contain', 'fund-tax-txt'];
+    dataMapMoObj.addIndexed(fundTax);
+  }
 }
